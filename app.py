926afebce84aaa5ca1401802890e879a20b70757
rogya33 <kei00216@gmail.com>
2023-11-10 12:09:24 +0900

    json route 제작

--- app.py
+++ app.py
@@ -2,10 +2,16 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+from collections import Counter
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '헤이모두들안녕내가누군지아니'
+    user_string = '헤이 헤이 모두들 모두들 모두들 안녕 안녕 내가 내가 내가 내가 누군지 아니 아니 아니 아니 아니'
+    counter = dict(Counter(user_string))
+    result = json.dumps(counter)
+
+    return result
 
