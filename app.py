617f4ef01d5e98ff367c1526bb231996a25d82ec
rogya33 <kei00216@gmail.com>
2023-11-10 12:28:49 +0900

    json route 제작

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template, request, Response
 from collections import Counter
 import json
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
@@ -15,3 +16,17 @@ def hello_world():
 
     return result
 
+
+@app.get('/count/')
+def count():
+    return render_template('count.html')
+
+
+@app.post('/result/')
+def result():
+    user_input = request.form['userinput']
+    word_dict = dict(Counter(user_input.split()))
+    result = json.dumps(word_dict)
+    return Response(result,
+                    mimetype='application/json',
+                    headers={'Content-Disposition': 'attachment;filename="count.json'})
